2a33a9bca5122a8920b20f14fe9a573fd50aed6f
rajeshit1421 <84392282+rajeshit1421@users.noreply.github.com>
2023-10-01 20:39:05 +0000

    updated code

--- basics_04/higherOrderFunc.js
+++ basics_04/higherOrderFunc.js
@@ -122,10 +122,10 @@ function findAllIndex(array, element) {
 var priceList = [10, 8, 2, 31, 10, 1, 65, 10];
 
 var occurance1 = findAllIndex(priceList, 10);
-console.log(occurance1); // [ 0, 4, 7 ]
+// console.log(occurance1); // [ 0, 4, 7 ]
 
 var occurance2 = findAllIndex(priceList, 8);
-console.log(occurance2); // [ 1 ]
+// console.log(occurance2); // [ 1 ]
 
 var occurance3 = findAllIndex(priceList, 9);
-console.log(occurance3); // []
+// console.log(occurance3); // []
